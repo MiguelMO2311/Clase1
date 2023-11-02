e307fb60aabd58d9f4effa9e091d8276d2d89d78
MiguelMO2311 <mmeneses73@gmail.com>
2023-11-02 21:03:26 +0100

    arreglos sobre el js

--- NodeProj.js
+++ NodeProj.js
@@ -1,39 +1,42 @@
 //  Reto 2. Tema 5 Funciones. 
 function sum(op1, op2, op3) {
-    result = op1 - op2 - op3
+    result = op1 + op2 + op3
+    return result;
 }
-sum(4, 2)
-console.log('sum');
-
+sum(1, 2, 3)
 
 
 function subs(op1, op2) {
     result = op1 - op2
+    return result;
 }
 subs(4, 2)
-console.log('subs');
+
 
 
 function mult(op1, op2) {
     result = op1 *= op2
+    return result;
 }
 mult(4, 2)
-console.log('mult')
+
 
 
 function div(op1, op2) {
     result = op1 /= op2
+    return result;
 }
 div(4, 2)
-console.log('div')
+
 
 
 function square(op1, op2) {
     op1 = op2
     result = op1 * op2
+    return result;
 }
 square(4, 4)
-console.log('square')
+
 
 
 module.exports = { sum, subs, mult, div, square }
